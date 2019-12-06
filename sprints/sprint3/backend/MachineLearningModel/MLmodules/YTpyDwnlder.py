49369061b1b26204bd12e6cbd900011c23868f58
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-12-05 19:37:11 -0800

    Create YTpyDwnlder.py
    In-progress module to download sound clips off youtube

---
+++ sprints/sprint3/backend/MachineLearningModel/MLmodules/YTpyDwnlder.py
@@ -0,0 +1,72 @@
+import pafy
+import time
+from playsound import playsound as ps
+
+link = input("Insert youtube link here: ")
+video = pafy.new(link)
+
+# Simple metadata retrieval:
+# Printing the title, rating, viewcount, author,
+# video length, duration, likes, dislikes, &
+# description
+print("Metadata: ")
+print(video.title)
+print(video.rating)
+print(video.viewcount)
+print(video.author, video.length)
+print(video.duration, video.likes, video.dislikes, video.description)
+
+# Gets all the stream types (1280, 640, etc.)
+print("Stream types:")
+streams = video.streams
+for s in streams:
+    print(s)
+
+
+# Downloading audio
+audiostream = video.getbestaudio()
+# audiostream.download()
+
+def replaceInvalidWinChars(string):
+    string = string.replace('"', '#')
+    string = string.replace('?', '#')
+    string = string.replace('|', '#')
+    string = string.replace('>', '#')
+    string = string.replace('<', '#')
+    string = string.replace(':', '#')
+    string = string.replace('*', '#')
+    return string
+
+from tkinter import filedialog as fd
+
+# Asking where to save it
+print("Select the directory...")
+dir_name = fd.askdirectory()
+print(dir_name)
+title = replaceInvalidWinChars(video.title)
+path = dir_name + "/" + video.title + ".wav"
+print(path)
+audiofile = audiostream.download(filepath=path)
+
+#import IPython.display as ipd
+
+# Playing back the audio
+#print("Playing back audio...")
+#ipd.Audio(filename=path)
+
+import librosa
+import librosa.display as ld
+import matplotlib.pyplot as plt
+
+# Visualization
+print('Loading data...')
+x, sr = librosa.load(path)
+plt.figure(figsize=(14, 5))
+ld.waveplot(x, sr=sr)
+
+print('Displaying data...')
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
