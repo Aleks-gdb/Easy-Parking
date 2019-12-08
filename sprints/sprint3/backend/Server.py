72ec75eaade35b0915cc37928303c745c453c55e
Aleks <jokerka17@gmail.com>
2019-12-08 13:45:11 -0800

    Clean the branch

--- sprints/sprint3/backend/Server.py
+++
@@ -1,9 +0,0 @@
-import ServerConnection
-import MachineLearning
-run = True
-while(run):
-    num = ServerConnection.openConnection() #Create a connection with the frontend
-    if (num == None):
-        run = False
-    else:
-        MachineLearning.mLearning(num) #Start the machine learning model and pass the number of iterations
